0caae6ff4325362579d3de80bd27315329b2e31c
Paul Merlin <paul@nosphere.org>
2010-04-11 14:18:11 +0200

    added netbeans code formatting settings to all projects

--- core/src/main/java/org/codeartisans/qipki/core/crypto/CryptIO.java
+++ core/src/main/java/org/codeartisans/qipki/core/crypto/CryptIO.java
@@ -40,75 +40,82 @@ import org.codeartisans.qipki.core.QiPkiFailure;
 import org.qi4j.api.composite.TransientComposite;
 import org.qi4j.api.mixin.Mixins;
 
-@Mixins(CryptIO.Mixin.class)
+@Mixins( CryptIO.Mixin.class )
 public interface CryptIO
-        extends TransientComposite {
+        extends TransientComposite
+{
 
-    PKCS10CertificationRequest readPKCS10PEM(Reader reader);
+    PKCS10CertificationRequest readPKCS10PEM( Reader reader );
 
-    CharSequence asPEM(X509Certificate certificate);
+    CharSequence asPEM( X509Certificate certificate );
 
-    CharSequence asPEM(PKCS10CertificationRequest pkcs10);
+    CharSequence asPEM( PKCS10CertificationRequest pkcs10 );
 
-    X509Extensions extractRequestedExtensions(PKCS10CertificationRequest pkcs10);
+    X509Extensions extractRequestedExtensions( PKCS10CertificationRequest pkcs10 );
 
     abstract class Mixin
-            implements CryptIO {
+            implements CryptIO
+    {
 
         @Override
-        public PKCS10CertificationRequest readPKCS10PEM(Reader reader) {
+        public PKCS10CertificationRequest readPKCS10PEM( Reader reader )
+        {
             try {
-                return (PKCS10CertificationRequest) new PEMReader(reader).readObject();
-            } catch (IOException ex) {
-                throw new IllegalArgumentException("Unable to read pkcs10 from PEM", ex);
+                return ( PKCS10CertificationRequest ) new PEMReader( reader ).readObject();
+            } catch ( IOException ex ) {
+                throw new IllegalArgumentException( "Unable to read pkcs10 from PEM", ex );
             }
         }
 
         @Override
-        public CharSequence asPEM(X509Certificate certificate) {
-            return asPEM(certificate.getClass().getSimpleName(), certificate);
+        public CharSequence asPEM( X509Certificate certificate )
+        {
+            return asPEM( certificate.getClass().getSimpleName(), certificate );
         }
 
         @Override
-        public CharSequence asPEM(PKCS10CertificationRequest pkcs10) {
-            return asPEM(pkcs10.getClass().getSimpleName(), pkcs10);
+        public CharSequence asPEM( PKCS10CertificationRequest pkcs10 )
+        {
+            return asPEM( pkcs10.getClass().getSimpleName(), pkcs10 );
         }
 
-        private CharSequence asPEM(String ilk, Object object) {
+        private CharSequence asPEM( String ilk, Object object )
+        {
             try {
                 StringWriter sw = new StringWriter();
-                PEMWriter pemWriter = new PEMWriter(sw);
-                pemWriter.writeObject(object);
+                PEMWriter pemWriter = new PEMWriter( sw );
+                pemWriter.writeObject( object );
                 pemWriter.flush();
                 return sw.getBuffer();
-            } catch (IOException ex) {
-                throw new QiPkiFailure("Unable to write " + ilk + " as PEM", ex);
+            } catch ( IOException ex ) {
+                throw new QiPkiFailure( "Unable to write " + ilk + " as PEM", ex );
             }
 
         }
 
         @Override
-        public X509Extensions extractRequestedExtensions(PKCS10CertificationRequest pkcs10) {
+        public X509Extensions extractRequestedExtensions( PKCS10CertificationRequest pkcs10 )
+        {
             final CertificationRequestInfo certificationRequestInfo = pkcs10.getCertificationRequestInfo();
             final ASN1Set attributesAsn1Set = certificationRequestInfo.getAttributes();
             // The `Extension Request` attribute is contained within an ASN.1 Set,
             // usually as the first element.
             X509Extensions certificateRequestExtensions = null;
-            for (int i = 0; i < attributesAsn1Set.size(); ++i) {
+            for ( int i = 0; i < attributesAsn1Set.size(); ++i ) {
                 // There should be only only one attribute in the set. (that is, only
                 // the `Extension Request`, but loop through to find it properly)
-                final DEREncodable derEncodable = attributesAsn1Set.getObjectAt(i);
-                if (derEncodable instanceof DERSequence) {
-                    final Attribute attribute = new Attribute((DERSequence) attributesAsn1Set.getObjectAt(i));
+                final DEREncodable derEncodable = attributesAsn1Set.getObjectAt( i );
+                if ( derEncodable instanceof DERSequence ) {
+                    final Attribute attribute = new Attribute( ( DERSequence ) attributesAsn1Set.getObjectAt( i ) );
 
-                    if (attribute.getAttrType().equals(PKCSObjectIdentifiers.pkcs_9_at_extensionRequest)) {
+                    if ( attribute.getAttrType().equals( PKCSObjectIdentifiers.pkcs_9_at_extensionRequest ) ) {
                         // The `Extension Request` attribute is present.
                         final ASN1Set attributeValues = attribute.getAttrValues();
 
                         // The X509Extensions are contained as a value of the ASN.1 Set.
                         // Assume that it is the first value of the set.
-                        if (attributeValues.size() >= 1) {
-                            certificateRequestExtensions = new X509Extensions((ASN1Sequence) attributeValues.getObjectAt(0));
+                        if ( attributeValues.size() >= 1 ) {
+                            certificateRequestExtensions = new X509Extensions( ( ASN1Sequence ) attributeValues.getObjectAt( 0 ) );
 
                             // No need to search any more.
                             break;
@@ -118,5 +125,7 @@ public interface CryptIO
             }
             return certificateRequestExtensions;
         }
+
     }
+
 }
