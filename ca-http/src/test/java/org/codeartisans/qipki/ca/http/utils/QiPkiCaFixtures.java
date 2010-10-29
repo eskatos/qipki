b0122e833b4ba904d7f7fbfa15fe50c83518e18a
Paul MERLIN <p.merlin@netheos.net>
2010-10-29 16:24:38 +0200

    Added LICENSE file, Netbeans code formatting configuration and fixed source headers

--- ca-http/src/test/java/org/codeartisans/qipki/ca/http/utils/QiPkiCaFixtures.java
+++ ca-http/src/test/java/org/codeartisans/qipki/ca/http/utils/QiPkiCaFixtures.java
@@ -1,23 +1,15 @@
 /*
- * Copyright (c) 2010 Paul Merlin <paul@nosphere.org>
+ * Copyright (c) 2010, Paul Merlin. All Rights Reserved.
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
  */
 package org.codeartisans.qipki.ca.http.utils;
 
@@ -49,17 +41,11 @@ public interface QiPkiCaFixtures
 {
 
     String KEYSTORE_NAME = "Test KeyStore";
-
     String ROOT_CA_NAME = "RootCA";
-
     String ROOT_CA_DN = "CN=root-test,OU=qipki,O=codeartisans";
-
     String USERS_CA_NAME = "UsersCA";
-
     String USERS_CA_DN = "CN=users-test,OU=qipki,O=codeartisans";
-
     String SERVICES_CA_NAME = "ServicesCA";
-
     String SERVICES_CA_DN = "CN=services-test,OU=qipki,O=codeartisans";
 
     abstract class Mixin
@@ -68,10 +54,8 @@ public interface QiPkiCaFixtures
 
         @Structure
         private UnitOfWorkFactory uowf;
-
         @Structure
         private ObjectBuilderFactory obf;
-
         @Service
         private CryptoValuesFactory cryptoValuesFactory;
 
