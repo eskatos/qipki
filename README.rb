1ef66a2d9e3a248f69a424117c2cf378ef62a65a
Paul Merlin <paul@nosphere.org>
2010-10-29 16:28:17 +0200

    Added github README.rb

---
+++ README.rb
@@ -0,0 +1,8 @@
+QiPKI
+=====
+
+QiPKI is a simple X.509 PKI implemented in Java using Qi4j.
+
+Each component is built as a library and has a http service as restfull as possible using json format for data structures.
+
+
