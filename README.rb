de955558d8504de5754e95133b372a2a54c0aeba
Paul Merlin <paul@nosphere.org>
2010-10-30 02:53:29 +0200

    Build refactor to isolate what will be released with 0.1 and what not

--- README.rb
+++ README.rb
@@ -5,4 +5,3 @@ QiPKI is a simple X.509 PKI implemented in Java using Qi4j.
 
 Each component is built as a library and has a http service as restfull as possible using json format for data structures.
 
-
