ebc2ad70b21503f818ae0886913877dd7db3f925
Paul MERLIN <p.merlin@netheos.net>
2010-11-09 18:10:03 +0100

    Added inconditionnal reindex on startup

---
+++ core/src/main/java/org/codeartisans/qipki/core/assembly/index/AutomaticReindexerService.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2010, Paul Merlin. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package org.codeartisans.qipki.core.assembly.index;
+
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.mixin.Mixins;
+import org.qi4j.api.service.Activatable;
+import org.qi4j.api.service.ServiceComposite;
+import org.qi4j.index.reindexer.ReindexerService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mixins( AutomaticReindexerService.Mixin.class )
+public interface AutomaticReindexerService
+        extends ServiceComposite, Activatable
+{
+
+    @SuppressWarnings( "PublicInnerClass" )
+    abstract class Mixin
+            implements AutomaticReindexerService
+    {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger( AutomaticReindexerService.class );
+        @Service
+        private ReindexerService reindexer;
+
+        @Override
+        public void activate()
+                throws Exception
+        {
+            LOGGER.debug( "Will start reindexing now.." );
+            long start = System.currentTimeMillis();
+            reindexer.reindex();
+            LOGGER.info( "Reindexing ended successfully and took {}ms", System.currentTimeMillis() - start );
+        }
+
+        @Override
+        public void passivate()
+                throws Exception
+        {
+        }
+
+    }
+
+}
